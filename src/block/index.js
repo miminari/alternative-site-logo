4e9feafcbf13e71f249dc3622ea43f97dde0d265
mimi <mimitips@gmail.com>
2021-12-06 18:21:15 +0900

    Remove / remove comments

--- src/block/index.js
+++ src/block/index.js
@@ -6,14 +6,12 @@ import { registerBlockType } from '@wordpress/blocks';
 /**
  * Internal dependencies
  */
-// import metadata from './block.json';
 import edit from './edit';
 import save from './save';
 
 import './style.scss';
 
 registerBlockType( 'alternative-site-logo/alternative-site-logo', {
-	// ...metadata,
 	edit,
 	save,
 } );
