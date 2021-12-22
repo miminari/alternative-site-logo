52930e967e1e55ab0510ab65e997b042569d3355
mimi <mimitips@gmail.com>
2021-12-22 10:58:36 +0900

    test / Add test 'can upload svg file'

--- alternative-site-logo.php
+++ alternative-site-logo.php
@@ -4,7 +4,7 @@
  * Description:       Example block written with ESNext standard and JSX support – build step required.
  * Requires at least: 5.8
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.0.1
  * Author:            Photosynthesic
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
--- tests/e2e/specs/add_block.test.js
+++ tests/e2e/specs/add_block.test.js
@@ -1,17 +1,50 @@
+/**
+ * External dependencies
+ */
+import path from 'path';
+import fs from 'fs';
+import os from 'os';
+import { v4 as uuid } from 'uuid';
+
+/**
+ * WordPress dependencies
+ */
 import {
 	createNewPost,
-	toggleGlobalBlockInserter,
 	insertBlock,
 	getEditedPostContent,
 } from '@wordpress/e2e-test-utils';
+
+async function upload( selector ) {
+	await page.waitForSelector( selector );
+	const inputElement = await page.$( selector );
+	const testImagePath = path.join(
+		__dirname,
+		'..',
+		'assets',
+		'upload_test_svg.svg'
+	);
+	const filename = uuid();
+	const tmpFileName = path.join( os.tmpdir(), filename + '.png' );
+	fs.copyFileSync( testImagePath, tmpFileName );
+	await inputElement.uploadFile( tmpFileName );
+	return filename;
+}
+
+async function waitForImage( filename ){
+	await page.waitForSelector(
+		'.wp-block-alternative-site-logo-alternative-site-logo svg'
+	);
+}
 
 describe('Alternative Site Logo', () => {
 	beforeEach(async () => {
 		await createNewPost();
 	});
-	it('should insert by name', async () => {
-		// await toggleGlobalBlockInserter();
+	it('can upload svg file', async () => {
 		await insertBlock('Alternative Site Logo');
-		expect(await getEditedPostContent()).toMatchSnapshot();
+		const filename = await upload(`.wp-block-alternative-site-logo-alternative-site-logo input[type=file]` );
+		// await waitForImage( filename );
+		expect( await getEditedPostContent() ).toMatchSnapshot();
 	});
 });
