fc1bed31786d9a8230b345928ee0713f0ec9b77d
mimi <mimitips@gmail.com>
2021-12-25 16:28:12 +0900

    WIP test / add changeSiteLang

--- tests/e2e/specs/add_block.test.js
+++ tests/e2e/specs/add_block.test.js
@@ -9,33 +9,22 @@ import { v4 as uuid } from 'uuid';
 /**
  * WordPress dependencies
  */
-import {
-	createNewPost,
-	insertBlock,
-	getEditedPostContent,
-} from '@wordpress/e2e-test-utils';
+import { createNewPost, insertBlock, getEditedPostContent } from '@wordpress/e2e-test-utils';
 
-async function upload( selector ) {
-	await page.waitForSelector( selector );
-	const inputElement = await page.$( selector );
-	const testImagePath = path.join(
-		__dirname,
-		'..',
-		'assets',
-		'upload_test_svg.svg'
-	);
+async function upload(selector) {
+	await page.waitForSelector(selector);
+	const inputElement = await page.$(selector);
+	const testImagePath = path.join(__dirname, '..', 'assets', 'upload_test_svg.svg');
 	const filename = uuid();
-	const tmpFileName = path.join( os.tmpdir(), filename + '.png' );
-	fs.copyFileSync( testImagePath, tmpFileName );
-	await inputElement.uploadFile( tmpFileName );
+	const tmpFileName = path.join(os.tmpdir(), filename + '.png');
+	fs.copyFileSync(testImagePath, tmpFileName);
+	await inputElement.uploadFile(tmpFileName);
 	return filename;
 }
 
-async function waitForImage( filename ){
-	await page.waitForSelector(
-		'.wp-block-alternative-site-logo-alternative-site-logo svg'
-	);
-}
+// async function waitForImage(filename) {
+// 	await page.waitForSelector('.wp-block-alternative-site-logo-alternative-site-logo svg');
+// }
 
 describe('Alternative Site Logo', () => {
 	beforeEach(async () => {
@@ -43,8 +32,9 @@ describe('Alternative Site Logo', () => {
 	});
 	it('can upload svg file', async () => {
 		await insertBlock('Alternative Site Logo');
-		const filename = await upload(`.wp-block-alternative-site-logo-alternative-site-logo input[type=file]` );
+		// const filename = await upload(
+		await upload(`.wp-block-alternative-site-logo-alternative-site-logo input[type=file]`);
 		// await waitForImage( filename );
-		expect( await getEditedPostContent() ).toMatchSnapshot();
+		expect(await getEditedPostContent()).toMatchSnapshot();
 	});
 });
