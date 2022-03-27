de58691233861856eb59133a9489d36eca488d85
mimi <mimitips@gmail.com>
2022-03-27 23:44:19 +0900

    env / Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,25 @@
-import { PlaywrightTestConfig, devices } from "@playwright/test";
+import path from 'path';
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     testMatch: '**/*.spec.ts',
     use: {
-        baseURL: 'http://localhost:8182',
+        baseURL: process.env.WP_BASE_URL || 'http://localhost:8182',
+        headless: true,
+        viewport: {
+			width: 960,
+			height: 700,
+		},
+        contextOptions: {
+			reducedMotion: 'reduce',
+			strictSelectors: true,
+		},
+        ignoreHTTPSErrors: true,
+        locale: 'en-US',
+        actionTimeout: 10_000, // 10 seconds.
         trace: 'on-first-retry',
     },
     projects: [
--- tests/e2e/specs/pw-sample.spec.ts
+++ tests/e2e/specs/pw-sample.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
     await page.goto('http://localhost:8182/');
-    const name = await page.innerText('.wp-block-site-title');
+    const name = await page.innerText('h1.wp-block-site-title');
     expect(name).toBe('alternative-site-logo');
 })
